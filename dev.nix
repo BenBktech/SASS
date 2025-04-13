b8d9b75ef5d1dbaebd25615922043bf9aa625339
BenBktech <bbrucher@gmail.com>
2025-04-13 17:34:48 +0200

    Updated: dev.nix

--- dev.nix
+++ dev.nix
@@ -1,6 +1,7 @@
-{
-  onCreate = ''
-    npm install
-    npm run build
-  '';
+{pkgs, ...}: {
+  idx.workspace.onCreate = {
+    npm-install = "npm install";
+    # files to open when the workspace is first opened.
+    default.openFiles = [ "app/layout.ts" ];
+  };
 }
